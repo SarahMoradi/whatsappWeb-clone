cc02ed4de3e9d9df6be2439bf8a6cba9607e8110
Sarah Moradi <moradisarah2001@gamil.com>
2021-08-24 18:02:34 +0430

    The order for displaying the profile menu was replaced

--- app.js
+++ app.js
@@ -1,44 +1,15 @@
 const option = document.getElementById("user-option");
-const optionMenu = document.querySelector(".menu-section");
+const optionMenu = document.querySelector(".back-nav-bar");
+const backButon = document.getElementById('back-btn');
 
 option.addEventListener("click", openProfile);
 
 function openProfile() {
-  optionMenu.innerHTML = `
-    
-    <div class="back-nav-bar">
-    <div class="top-nav">
-      <div class="nav-button">
-        <i id="back-btn" class="material-icons back-icon" style="font-size:25px ; color: #fff;">arrow_back</i>
-        <p class="header-nav">Profile</p>
-      </div>
-    </div>
-    <div class="choose-profile">
-        <img src="picture/profile-picture.jpg" class="picture"/>
-    </div>
-    <div class="username-profile">
-      <p class="head-user">Your Name</p>
-      <div class="edit-username">
-        <p class="ed-name">Username</p>
-        <i class='fas fa-pen pen-icon'></i>
-      </div>
-    </div>
-    <div class="description-username">
-      <p>
-        This is not your username or pin.
-        This name will be visible to your WhatsApp contacts.
-      </p>
-    </div>
-
-    <div class="aboutUs-profile">
-      <p class="about-user">About</p>
-      <div class="edit-about">
-        <p class="ed-status">Status</p>
-        <i class='fas fa-pen pen-icon-status'></i>
-      </div>
-    </div>
-    <div class="footer"></div>
-  </div>
-</div>
-    `;
+  optionMenu.style.display = 'block';
+ 
 }
+
+backButon.addEventListener('click',function(){
+  optionMenu.style.display = 'none';
+  
+})
