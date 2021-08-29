fe7219c9968be40196bca72b0bfe20c63ec8c75c
Sarah Moradi <moradisarah2001@gamil.com>
2021-08-30 02:29:40 +0430

    Set the style to the main file

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const option = document.getElementById("user-option");
 const optionMenu = document.querySelector(".back-nav-bar");
 const backButon = document.getElementById('back-btn');
 
-option.addEventListener("click", openProfile);
+const picture = document.querySelector(".picture");
+const optionProfile = document.getElementById("optionProfile");
 
+option.addEventListener("click", openProfile);
 function openProfile() {
   optionMenu.style.display = 'block';
  
 }
-
 backButon.addEventListener('click',function(){
   optionMenu.style.display = 'none';
+  optionProfile.style.display = 'none';
   
 })
+
+picture.addEventListener('click', function(){
+ optionProfile.style.display = 'block';
+});
